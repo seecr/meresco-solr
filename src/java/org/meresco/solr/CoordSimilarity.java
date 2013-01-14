953327bf1e9d6f6c749215e2cb0fb9868de3be57
Hendrik Moree <hendrik@seecr.nl>
2013-01-14 16:59:03 +0100

    JPM/HM: Custom Similarity class with ranking based on matching terms in query

--- src/java/org/meresco/solr/CoordSimilarity.java
+++ src/java/org/meresco/solr/CoordSimilarity.java
@@ -1,7 +1,33 @@
 package org.meresco.solr;
 
 import org.apache.lucene.search.similarities.DefaultSimilarity;
+import org.apache.lucene.index.FieldInvertState;
+import org.apache.lucene.index.Norm;
 
 public class CoordSimilarity extends DefaultSimilarity {
 
+    @Override
+    public float idf(long docFreq, long numDocs) {
+        return 1;
+    }
+
+    @Override
+    public float queryNorm(float sumOfSquaredWeights) {
+        return 1;
+    }
+
+    @Override
+    public float tf(float freq) {
+        return 1;
+    }
+
+    @Override
+    public float coord(int overlap, int maxOverlap) {
+        return 1;
+    }
+    
+    @Override
+    public void computeNorm(FieldInvertState state, Norm norm) {
+
+    }
 }
--- test/_integration/solrshardingtest.py
+++ test/_integration/solrshardingtest.py
@@ -6,15 +6,17 @@ class SolrShardingTest(IntegrationTestCase):
     def testSortingOnRelevancyWithShards(self):
         header, body = getRequest(port=self.solr1.solrPort,
                 path="/solr/records/select", 
-                arguments= { "q": '__all__:fiets', 'fl': '__id__,score'})
+                arguments= { "q": '__all__:fiets', 'fl': '__id__,score', "debugQuery": "on"})
         self.assertEquals(['record:0004', 'record:0001'], body.xpath('//doc/str[@name="__id__"]/text()'))
-        self.assertEquals(['0.37158427', '0.18579213'], body.xpath('//doc/float[@name="score"]/text()'))
+        # self.assertEquals(['0.37158427', '0.18579213'], body.xpath('//doc/float[@name="score"]/text()'))
+        self.assertEquals(['0.22091983', '0.11045992'], body.xpath('//doc/float[@name="score"]/text()'))
 
         header, body = getRequest(port=self.solr2.solrPort,
                 path="/solr/records/select", 
                 arguments= { "q": '__all__:fiets', 'fl': '__id__,score'})
         self.assertEquals(['record:0003', 'record:0002'], body.xpath('//doc/str[@name="__id__"]/text()'))
-        self.assertEquals(['0.32180142', '0.26274976'], body.xpath('//doc/float[@name="score"]/text()'))
+        # self.assertEquals(['0.32180142', '0.26274976'], body.xpath('//doc/float[@name="score"]/text()'))
+        self.assertEquals(['0.19132218', '0.15621391'], body.xpath('//doc/float[@name="score"]/text()'))
 
         header, body = getRequest(port=self.solr1.solrPort,
                 path="/solr/records/select", 
@@ -24,25 +26,41 @@ class SolrShardingTest(IntegrationTestCase):
         self.assertEquals(['record:0004', 'record:0003', 'record:0002', 'record:0001'], body.xpath('//doc/str[@name="__id__"]/text()'))
 
     def testSortingOnRelevancyWithORquery(self):
-        header, body = getRequest(parse=False, port=self.solr1.solrPort,
+        header, body = getRequest(port=self.solr1.solrPort,
                 path="/solr/records/select", 
                 arguments= { "q": 'zadel OR stuur OR wiel OR ketting', 'fl': '__id__,score'})
-        #print body
+        self.assertEquals(['record:0004', 'record:0001'], body.xpath('//doc/str[@name="__id__"]/text()'))
+        self.assertEquals(['1.0479599', '0.11045992'], body.xpath('//doc/float[@name="score"]/text()'))
 
-        header, body = getRequest(parse=False, port=self.solr2.solrPort,
+        header, body = getRequest(port=self.solr2.solrPort,
                 path="/solr/records/select", 
                 arguments= { "q": 'zadel OR stuur OR wiel OR ketting', 'fl': '__id__,score'})
-        #print body
+        self.assertEquals(['record:0003', 'record:0002'], body.xpath('//doc/str[@name="__id__"]/text()'))
+        self.assertEquals(['0.53341985', '0.22091983'], body.xpath('//doc/float[@name="score"]/text()'))
 
         header, body = getRequest(port=self.solr2.solrPort,
                 path="/solr/records/select", 
                 arguments= { "q": 'zadel OR stuur OR wiel OR ketting', 'fl': '__id__,score',
                     "shards": 'localhost:%s/solr/records,localhost:%s/solr/records' % (self.solr1.solrPort, self.solr2.solrPort)})
         self.assertEquals(['record:0004', 'record:0003', 'record:0002', 'record:0001'], body.xpath('//doc/str[@name="__id__"]/text()'))
-        self.assertEquals(['0.57226515', '0.18706693', '0.051650114', '0.0150798615'], body.xpath('//doc/float[@name="score"]/text()'))
+        # self.assertEquals(['0.57226515', '0.18706693', '0.051650114', '0.0150798615'], body.xpath('//doc/float[@name="score"]/text()'))
+        self.assertEquals(['1.0479599', '0.53341985', '0.22091983', '0.11045992'], body.xpath('//doc/float[@name="score"]/text()'))
 
     def testTryToGetScoresOnCoordinationFactorOnlyAndNotTFIDF(self):
-        header, body = getRequest(parse=False, port=self.solr1.solrPort,
+        header, body = getRequest(port=self.solr1.solrPort,
                 path="/solr/records/select", 
-                arguments= { "debugQuery": "on", "q": 'zadel OR stuur OR wiel OR ketting', 'fl': '__id__,score'})
-        print body
+                arguments= { "q": '__all_similarity__:zadel OR __all_similarity__:stuur OR __all_similarity__:wiel OR __all_similarity__:ketting', 'fl': '__id__,score'})
+        self.assertEquals(['record:0004', 'record:0001'], body.xpath('//doc/str[@name="__id__"]/text()'))
+        self.assertEquals(['4.0', '1.0'], body.xpath('//doc/float[@name="score"]/text()'))
+
+        header, body = getRequest(port=self.solr2.solrPort,
+                path="/solr/records/select", 
+                arguments= { "q": '__all_similarity__:zadel OR __all_similarity__:stuur OR __all_similarity__:wiel OR __all_similarity__:ketting', 'fl': '__id__,score'})
+        self.assertEquals(['record:0003', 'record:0002'], body.xpath('//doc/str[@name="__id__"]/text()'))
+        self.assertEquals(['3.0', '2.0'], body.xpath('//doc/float[@name="score"]/text()'))
+
+        header, body = getRequest(port=self.solr1.solrPort,
+                path="/solr/records/select", 
+                arguments= { "q": '__all_similarity__:zadel OR __all_similarity__:stuur OR __all_similarity__:wiel OR __all_similarity__:ketting', 'fl': '__id__,score',
+                    "shards": 'localhost:%s/solr/records,localhost:%s/solr/records' % (self.solr1.solrPort, self.solr2.solrPort)})
+        self.assertEquals(['4.0', '3.0', '2.0', '1.0'], body.xpath('//doc/float[@name="score"]/text()'))
