45de852d70d1d9be3ca650e60986ee641191407c
Hendrik Moree <hendrik@seecr.nl>
2013-04-19 11:00:11 +0200

    JPM/HM: Updated license

--- src/java/org/meresco/solr/JoinParserPlugin.java
+++ src/java/org/meresco/solr/JoinParserPlugin.java
@@ -1,3 +1,29 @@
+/* begin license *
+ *
+ * "Meresco Solr" is a set of components and tools
+ *  to integrate Solr into "Meresco."
+ *
+ * Copyright (C) 2013 Seecr (Seek You Too B.V.) http://seecr.nl
+ * Copyright (C) 2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+ *
+ * This file is part of "Meresco Solr"
+ *
+ * "Meresco Solr" is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * "Meresco Solr" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Meresco Solr"; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * end license */
+
 package org.meresco.solr;
 
 import org.apache.lucene.search.Query;
@@ -29,10 +55,7 @@ public class JoinParserPlugin extends QParserPlugin {
                 String localQueryString = localParams.get("v");
 
                 CoreDescriptor coreDescriptor = req.getCore().getCoreDescriptor();
-                if (otherCoreName.equals(coreDescriptor.getName()) ) {
-                    query = subQuery(localQueryString, null).getQuery();	
-                }
-                else {
+                if (!otherCoreName.equals(coreDescriptor.getName()) ) {
                     CoreContainer container = coreDescriptor.getCoreContainer();
                     final SolrCore otherCore = container.getCore(otherCoreName);
                     if (otherCore == null) {
@@ -49,7 +72,6 @@ public class JoinParserPlugin extends QParserPlugin {
 class JoinQuery extends Query {
 	String coreName;
     String localQueryString;
-    Query query = null;
 
     public JoinQuery(String coreName, String localQueryString) {
         this.coreName = coreName;
--- test/alltests.sh
+++ test/alltests.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 ## begin license ##
-# 
+#
 # "Meresco Solr" is a set of components and tools
-#  to integrate Solr into "Meresco." 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+#  to integrate Solr into "Meresco."
+#
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 SURF http://www.surf.nl
-# 
+# Copyright (C) 2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+#
 # This file is part of "Meresco Solr"
-# 
+#
 # "Meresco Solr" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Solr" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Solr"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
+
 set -o errexit
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
