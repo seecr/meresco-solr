3e8c82b5eff97c5819639a64528c876f1f73819f
Seecr Development Team <development@seecr.nl>
2013-04-26 12:25:48 +0200

    JPM: removed more unused imports

--- src/java/org/meresco/solr/Uid2DocIdsMap.java
+++ src/java/org/meresco/solr/Uid2DocIdsMap.java
@@ -29,10 +29,8 @@ package org.meresco.solr;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.lucene.search.FieldCache;
 import org.apache.solr.common.SolrException;
