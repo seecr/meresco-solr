b578e88a5fe684499e18c99f04c6c3a53b408661
Seecr Development Team <development@seecr.nl>
2013-07-15 14:04:16 +0200

    HM/TJ: Convert cql filterqueries to lucene queries

--- meresco/solr/cql2solrlucenequery.py
+++ meresco/solr/cql2solrlucenequery.py
@@ -1,27 +1,29 @@
 ## begin license ##
-# 
-# "Meresco Solr" is a set of components and tools
-#  to integrate Solr into "Meresco." 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# "NBC+" also known as "ZP (ZoekPlatform)" is
+#  initiated by Stichting Bibliotheek.nl to provide a new search service
+#  for all public libraries in the Netherlands.
+#
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 SURF http://www.surf.nl
-# 
-# This file is part of "Meresco Solr"
-# 
-# "Meresco Solr" is free software; you can redistribute it and/or modify
+# Copyright (C) 2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+#
+# This file is part of "NBC+ (Zoekplatform BNL)"
+#
+# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# "Meresco Solr" is distributed in the hope that it will be useful,
+#
+# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Solr"; if not, write to the Free Software
+# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from meresco.core import Observable
@@ -40,6 +42,8 @@ class Cql2SolrLuceneQuery(Observable, Logger):
         joinQueries = kwargs.get('joinQueries', [])
         for jq in joinQueries:
             jq['query'] = self._convert(jq['query'])
+        filterQueries = kwargs.get('filterQueries', [])
+        filterQueries = [self._convert(fq) for fq in filterQueries]
         response = yield self.any.executeQuery(luceneQueryString=self._convert(cqlAbstractSyntaxTree), *args, **kwargs)
         raise StopIteration(response)
 
--- test/cql2solrlucenequerytest.py
+++ test/cql2solrlucenequerytest.py
@@ -1,27 +1,29 @@
 ## begin license ##
-# 
-# "Meresco Solr" is a set of components and tools
-#  to integrate Solr into "Meresco." 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# "NBC+" also known as "ZP (ZoekPlatform)" is
+#  initiated by Stichting Bibliotheek.nl to provide a new search service
+#  for all public libraries in the Netherlands.
+#
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 SURF http://www.surf.nl
-# 
-# This file is part of "Meresco Solr"
-# 
-# "Meresco Solr" is free software; you can redistribute it and/or modify
+# Copyright (C) 2013 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
+#
+# This file is part of "NBC+ (Zoekplatform BNL)"
+#
+# "NBC+ (Zoekplatform BNL)" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# "Meresco Solr" is distributed in the hope that it will be useful,
+#
+# "NBC+ (Zoekplatform BNL)" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Solr"; if not, write to the Free Software
+# along with "NBC+ (Zoekplatform BNL)"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from unittest import TestCase
@@ -73,6 +75,9 @@ class Cql2SolrLuceneQueryTest(TestCase):
 
     def testJoinQueries(self):
         self.assertConversion(['term2', 'term1'], query='term1', joinQueries=[dict(someKey='someValue', query=parseString('term2'))])
+
+    def testFilterQueries(self):
+        self.assertConversion(['term2', 'term1'], query='term1', filterQueries=[parseString('term2')])
 
 
     def assertConversion(self, expectedClauses, query, **kwargs):
